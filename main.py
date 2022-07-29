9c7dc97eda7fd82586b0ea6efae4687a97095a03
DarkLord76865 <40371578+darklord76865@users.noreply.github.com>
2022-07-29 22:20:22 +0200

    Update

--- main.py
+++ main.py
@@ -6,111 +6,95 @@ import os
 import sys
 from dirsync import sync
 import filecmp
-from multiprocessing import Process
+from threading import Thread
+import psutil
 
+
+def are_dir_trees_equal(dir1, dir2):
+	"""
+	Compare two directories recursively. Files in each directory are
+	assumed to be equal if their names and contents are equal.
+
+	@param dir1: First directory path
+	@param dir2: Second directory path
+
+	@return: True if the directory trees are the same and
+		there were no errors while accessing the directories or files,
+		False otherwise.
+	"""
+
+	dirs_cmp = filecmp.dircmp(dir1, dir2)
+	if len(dirs_cmp.left_only) > 0 or len(dirs_cmp.right_only) > 0 or len(dirs_cmp.funny_files) > 0:
+		return False
+	(_, mismatch, errors) = filecmp.cmpfiles(dir1, dir2, dirs_cmp.common_files, shallow=False)
+	if len(mismatch) > 0 or len(errors) > 0:
+		return False
+	for common_dir in dirs_cmp.common_dirs:
+		new_dir1 = os.path.join(dir1, common_dir)
+		new_dir2 = os.path.join(dir2, common_dir)
+		if not are_dir_trees_equal(new_dir1, new_dir2):
+			return False
+	return True
 
 def syncing():
-	exit_btn.config(state="disabled")
-	sync_btn.config(state="disabled")
-	source_txt.config(state="disabled")
-	target_txt.config(state="disabled")
-	browse1_btn.config(state="disabled")
-	browse2_btn.config(state="disabled")
+	global started
 
 	source_dir = source_txt.get()
 	target_dir = target_txt.get()
 
-	if not os.path.exists(source_dir) and not os.path.exists(target_dir):
+	not_existing = False
+	if not os.path.exists(source_dir):
+		not_existing = True
 		source_txt.delete(0, END)
+	if not os.path.exists(target_dir):
+		not_existing = True
 		target_txt.delete(0, END)
-		messagebox.showerror("Existence", "The Source and Destination folders don't exist!")
-		return
-	else:
-		if os.path.exists(source_dir):
-			pass
-		else:
-			source_txt.delete(0, END)
-			messagebox.showerror("Existence", "The Source folder doesn't exist!")
-			return
-		if os.path.exists(target_dir):
-			pass
-		else:
-			target_txt.delete(0, END)
-			messagebox.showerror("Existence", "The Destination folder doesn't exist!")
-			return
-
-	quest = messagebox.askyesno("Files",
-	                            "Running this program will delete any aditional files that are in Destination folder and not in Source folder.\nAre you sure you want to proceed?")
-	if quest:
-		pass
-	else:
+	if not_existing:
+		messagebox.showerror(title="Error!", message="Invalid folder selected!", parent=root)
 		return
 
-	working_lbl.place(width=100, height=25, x=200, y=240)
+	if not messagebox.askyesno(title="Mirror", message="Running this program will delete any files that are in destination, but not in source folder.\nAre you sure you want to proceed?"):
+		return
 
-	loop = True
+	started = True
+	syncing_lbl.config(text="Mirroring...")
+	browse1_btn.config(background="grey80", activebackground="grey80", highlightthickness=2)
+	browse2_btn.config(background="grey80", activebackground="grey80", highlightthickness=2)
+	source_txt.config(state="disabled")
+	target_txt.config(state="disabled")
+	sync_btn.config(background="grey80", activebackground="grey80", highlightthickness=2)
+
 	num = 0
-	loop_info = None
-	while loop and num <= 3:
-		num = num + 1
-		sync(source_dir, target_dir, 'sync', purge=True)
+	while num < 3:
+		num += 1
 
-		def are_dir_trees_equal(dir1, dir2):
-			"""
-			Compare two directories recursively. Files in each directory are
-			assumed to be equal if their names and contents are equal.
+		sync(source_dir, target_dir, 'sync', purge=True, force=True, create=True)
 
-			@param dir1: First directory path
-			@param dir2: Second directory path
-
-			@return: True if the directory trees are the same and
-				there were no errors while accessing the directories or files,
-				False otherwise.
-			"""
-
-			dirs_cmp = filecmp.dircmp(dir1, dir2)
-			if len(dirs_cmp.left_only) > 0 or len(dirs_cmp.right_only) > 0 or \
-					len(dirs_cmp.funny_files) > 0:
-				return False
-			(_, mismatch, errors) = filecmp.cmpfiles(
-				dir1, dir2, dirs_cmp.common_files, shallow=False)
-			if len(mismatch) > 0 or len(errors) > 0:
-				return False
-			for common_dir in dirs_cmp.common_dirs:
-				new_dir1 = os.path.join(dir1, common_dir)
-				new_dir2 = os.path.join(dir2, common_dir)
-				if not are_dir_trees_equal(new_dir1, new_dir2):
-					return False
-			return True
-
-		var = are_dir_trees_equal(source_dir, target_dir)
-		if var:
-			loop = False
+		if are_dir_trees_equal(source_dir, target_dir):
 			loop_info = True
+			break
 		else:
 			loop_info = False
 
-	working_lbl.place_forget()
-
-	if loop_info:
-		messagebox.showinfo("Completed", "Folders were synchronised successfully!")
-	else:
-		messagebox.showerror("Failed",
-		                     "Synchronising folders failed, check folder and file attributes and try again!")
-
-	exit_btn.config(state="normal")
-	sync_btn.config(state="normal")
+	started = True
+	syncing_lbl.config(text="")
+	browse1_btn.config(background="grey95", activebackground="grey95", highlightthickness=2)
+	browse2_btn.config(background="grey95", activebackground="grey95", highlightthickness=2)
 	source_txt.config(state="normal")
 	target_txt.config(state="normal")
-	browse1_btn.config(state="normal")
-	browse2_btn.config(state="normal")
+	sync_btn.config(background="grey95", activebackground="grey95", highlightthickness=2)
+
+	if loop_info:
+		messagebox.showinfo("Completed!", "Folder was mirrored successfully!", parent=root)
+	else:
+		messagebox.showerror("Failed!", "Mirroring failed!", parent=root)
 
 def sync_click(event):
 	global sync_process, started
 	if not started:
 		started = True
 
-		sync_process = Process(target=syncing)
+		sync_process = Thread(target=syncing)
 		sync_process.start()
 
 def exit_click(event=None):
@@ -119,13 +103,7 @@ def exit_click(event=None):
 		root.destroy()
 	except _tkinter.TclError:
 		pass
-	try:
-		sync_process.kill()
-		sync_process.join()
-		sync_process.close()
-	except AttributeError:
-		pass
-	sys.exit()
+	psutil.Process(os.getpid()).kill()
 
 def browse_click(event, ent):
 	folder = filedialog.askdirectory(parent=root, initialdir=os.getcwd())
@@ -139,6 +117,12 @@ def change_thickness(event, widget, typ, t_l, t_h, started):
 			widget.config(highlightthickness=t_l)
 		else:
 			widget.config(highlightthickness=t_h)
+
+def change_exit_thickness(event, widget, typ, t_l, t_h):
+	if typ:
+		widget.config(highlightthickness=t_l)
+	else:
+		widget.config(highlightthickness=t_h)
 
 def resource_path(relative_path=""):
 	""" Get absolute path to resource, works for dev and for PyInstaller """
@@ -191,17 +175,20 @@ if __name__ == '__main__':
 	browse2_btn.bind("<Enter>", lambda event: change_thickness(event, browse2_btn, False, 2, 4, started))
 	browse2_btn.bind("<Leave>", lambda event: change_thickness(event, browse2_btn, True, 2, 4, started))
 
-	exit_btn = Label(root, text="Quit", font=("Segoe UI", 10, "bold"), anchor=CENTER, justify=CENTER, background="grey95", activeforeground="grey95", highlightthickness=2, highlightcolor="#000000", highlightbackground="#000000", borderwidth=0)
+	exit_btn = Label(root, text="Quit", font=("Segoe UI", 10, "bold"), anchor=CENTER, justify=CENTER, background="grey95", activeforeground="grey95", highlightthickness=2, highlightcolor="red", highlightbackground="red", borderwidth=0)
 	exit_btn.place(y=180, width=100, x=40, height=30)
 	exit_btn.bind("<ButtonRelease-1>", exit_click)
-	exit_btn.bind("<Enter>", lambda event: change_thickness(event, exit_btn, False, 2, 4, started))
-	exit_btn.bind("<Leave>", lambda event: change_thickness(event, exit_btn, True, 2, 4, started))
+	exit_btn.bind("<Enter>", lambda event: change_exit_thickness(event, exit_btn, False, 2, 4))
+	exit_btn.bind("<Leave>", lambda event: change_exit_thickness(event, exit_btn, True, 2, 4))
 
 	sync_btn = Label(root, text="Mirror", font=("Segoe UI", 10, "bold"), anchor=CENTER, justify=CENTER, background="grey95", activeforeground="grey95", highlightthickness=2, highlightcolor="#000000", highlightbackground="#000000", borderwidth=0)
 	sync_btn.place(y=180, width=100, x=360, height=30)
 	sync_btn.bind("<ButtonRelease-1>", sync_click)
 	sync_btn.bind("<Enter>", lambda event: change_thickness(event, sync_btn, False, 2, 4, started))
 	sync_btn.bind("<Leave>", lambda event: change_thickness(event, sync_btn, True, 2, 4, started))
+
+	syncing_lbl = Label(root, text="", font=("Segoe UI", 9, "italic", "bold"), anchor=CENTER, justify=CENTER, foreground="#000000", activeforeground="#000000", background="#ffffff", activebackground="#ffffff", highlightthickness=0, borderwidth=0)
+	syncing_lbl.place(height=30, width=100, x=200, y=180)
 
 	root.mainloop()
 
