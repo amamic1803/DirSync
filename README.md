eefb16f5780a8dcc9df92514c361eb1a83f4ea5f
DarkLord76865 <mmima191@gmail.com>
2023-12-10 00:49:00 +0100

    update 1.1.1

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 # DirSync
 A program to mirror a selected folder to another location
 
-
 ![screenshot](https://user-images.githubusercontent.com/40371578/181839613-d9ccd296-14d3-408d-af6e-f3cdaa8c2c61.png)
 
+This is very simple program with basic functionality.
+For serious file synchronization tasks, I recommend using a more advanced program.
+Check out [FreeFileSync](https://freefilesync.org/)
 
 ## Downloading
 Prebuilt program is available under Releases
 
 ## Building
 1. Clone repository
-2. Run command: `pip install -r requirements.txt`
-3. Run build.py
-4. Built `*.exe` will be placed in the same folder
+2. Install Python
+3. Install dependencies: `pip install -r requirements.txt`
+4. Install UPX (optional)
+5. Run build.py
+6. Built `*.exe` will be placed in the same folder
--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 import filecmp
 import os
 import sys
+import tkinter as tk
 from logging import Logger
 from threading import Thread
-from tkinter import *
 from tkinter import filedialog
 from tkinter import messagebox
 
 import _tkinter
-import psutil
 from dirsync import sync
 
+
+def resource_path(relative_path=""):
+	""" Get absolute path to resource, works for dev and for PyInstaller """
+	try:
+		# PyInstaller creates a temp folder and stores path in _MEIPASS
+		base_path = sys._MEIPASS
+	except Exception:
+		base_path = os.path.abspath(".")
+	return os.path.join(base_path, relative_path)
 
 def are_dir_trees_equal(dir1, dir2):
 	"""
@@ -38,169 +46,188 @@ def are_dir_trees_equal(dir1, dir2):
 			return False
 	return True
 
-def syncing():
-	global started
 
-	source_dir = source_txt.get()
-	target_dir = target_txt.get()
+class App:
+	def __init__(self):
+		self.started = False
+		self.sync_thread = None
 
-	not_existing = False
-	if not os.path.exists(source_dir):
-		not_existing = True
-		source_txt.delete(0, END)
-	if not os.path.exists(target_dir):
-		not_existing = True
-		target_txt.delete(0, END)
-	if not_existing:
-		messagebox.showerror(title="Error!", message="Invalid folder selected!", parent=root)
-		return
+		self.root = tk.Tk()
+		self.root.title("DirSync")
+		self.root.geometry(f"500x230"
+		                   f"+{self.root.winfo_screenwidth() // 2 - 250}"
+		                   f"+{self.root.winfo_screenheight() // 2 - 115}")
+		self.root.resizable(False, False)
+		self.root.iconbitmap(resource_path("resources/dir-icon.ico"))
+		self.root.config(background="#ffffff")
 
-	if not messagebox.askyesno(title="Mirror", message="Running this program will delete any files that are in destination, but not in source folder.\nAre you sure you want to proceed?"):
-		return
+		self.title_lbl = tk.Label(self.root, text="DirSync", font=("Times New Roman Bold", 35),
+		                          anchor=tk.CENTER, justify=tk.CENTER,
+		                          foreground="#000000", activeforeground="#000000",
+		                          background="#ffffff", activebackground="#ffffff",
+		                          highlightthickness=0, borderwidth=0)
+		self.title_lbl.place(width=500, height=70, x=0, y=0)
 
-	started = True
-	syncing_lbl.config(text="Mirroring...")
-	browse1_btn.config(background="grey80", activebackground="grey80", highlightthickness=2)
-	browse2_btn.config(background="grey80", activebackground="grey80", highlightthickness=2)
-	source_txt.config(state="disabled")
-	target_txt.config(state="disabled")
-	sync_btn.config(background="grey80", activebackground="grey80", highlightthickness=2)
+		self.description_lbl = tk.Label(self.root, text="Mirror source folder to destination folder",
+		                                font=("Segoe UI", 9, "italic", "bold"), anchor=tk.CENTER, justify=tk.CENTER,
+		                                foreground="#000000", activeforeground="#000000", background="#ffffff",
+		                                activebackground="#ffffff", highlightthickness=0, borderwidth=0)
+		self.description_lbl.place(height=20, width=500, x=0, y=80)
 
-	num = 0
-	while num < 3:
-		num += 1
+		self.source_lbl = tk.Label(self.root, text="Source:", font=("Segoe UI", 10, "bold"),
+		                           anchor=tk.CENTER, justify=tk.CENTER,
+		                           foreground="#000000", activeforeground="#000000",
+		                           background="#ffffff", activebackground="#ffffff",
+		                           highlightthickness=0, borderwidth=0)
+		self.source_lbl.place(width=90, height=25, x=0, y=105)
 
-		sync(source_dir, target_dir, 'sync', logger=Logger(name="capture-stdout"), purge=True, force=True, create=True)
+		self.destination_lbl = tk.Label(self.root, text="Destination:", font=("Segoe UI", 10, "bold"),
+		                                anchor=tk.CENTER, justify=tk.CENTER,
+		                                foreground="#000000", activeforeground="#000000",
+		                                background="#ffffff", activebackground="#ffffff",
+		                                highlightthickness=0, borderwidth=0)
+		self.destination_lbl.place(width=90, height=25, x=0, y=135)
 
-		if are_dir_trees_equal(source_dir, target_dir):
-			loop_info = True
-			break
+		self.source_txt = tk.Entry(self.root, font=("Segoe UI", 9), justify=tk.LEFT,
+		                           highlightthickness=1, highlightcolor="grey50",
+		                           highlightbackground="grey50", disabledbackground="grey80",
+		                           foreground="#000000", background="grey95", borderwidth=0)
+		self.source_txt.place(width=320, height=25, x=90, y=105)
+
+		self.target_txt = tk.Entry(self.root, font=("Segoe UI", 9), justify=tk.LEFT,
+		                           highlightthickness=1, highlightcolor="grey50",
+		                           highlightbackground="grey50", disabledbackground="grey80",
+		                           foreground="#000000", background="grey95", borderwidth=0)
+		self.target_txt.place(width=320, height=25, x=90, y=135)
+
+		self.browse1_btn = tk.Label(self.root, text="Browse", font=("Segoe UI", 9), cursor="hand2",
+		                            anchor=tk.CENTER, justify=tk.CENTER,
+		                            background="grey95", activeforeground="grey95", highlightthickness=2,
+		                            highlightcolor="#000000", highlightbackground="#000000", borderwidth=0)
+		self.browse1_btn.place(y=105, width=80, x=415, height=25)
+		self.browse1_btn.bind("<ButtonRelease-1>", lambda event: self.browse_click(self.source_txt))
+		self.browse1_btn.bind("<Enter>", lambda event: self.browse1_btn.config(highlightthickness=4) if not self.started else None)
+		self.browse1_btn.bind("<Leave>", lambda event: self.browse1_btn.config(highlightthickness=2) if not self.started else None)
+
+		self.browse2_btn = tk.Label(self.root, text="Browse", font=("Segoe UI", 9), cursor="hand2",
+		                            anchor=tk.CENTER, justify=tk.CENTER,
+		                            background="grey95", activeforeground="grey95", highlightthickness=2,
+		                            highlightcolor="#000000", highlightbackground="#000000", borderwidth=0)
+		self.browse2_btn.place(y=135, width=80, x=415, height=25)
+		self.browse2_btn.bind("<ButtonRelease-1>", lambda event: self.browse_click(self.target_txt))
+		self.browse2_btn.bind("<Enter>", lambda event: self.browse2_btn.config(highlightthickness=4) if not self.started else None)
+		self.browse2_btn.bind("<Leave>", lambda event: self.browse2_btn.config(highlightthickness=2) if not self.started else None)
+
+		self.exit_btn = tk.Label(self.root, text="Quit", font=("Segoe UI", 10, "bold"), cursor="hand2",
+		                         anchor=tk.CENTER, justify=tk.CENTER,
+		                         background="grey95", activeforeground="grey95", highlightthickness=2,
+		                         highlightcolor="red", highlightbackground="red", borderwidth=0)
+		self.exit_btn.place(y=180, width=100, x=40, height=30)
+		self.exit_btn.bind("<ButtonRelease-1>", lambda event: self.quit())
+		self.exit_btn.bind("<Enter>", lambda event: self.exit_btn.config(highlightthickness=4))
+		self.exit_btn.bind("<Leave>", lambda event: self.exit_btn.config(highlightthickness=2))
+
+		self.sync_btn = tk.Label(self.root, text="Mirror", font=("Segoe UI", 10, "bold"), cursor="hand2",
+		                         anchor=tk.CENTER, justify=tk.CENTER,
+		                         background="grey95", activeforeground="grey95", highlightthickness=2,
+		                         highlightcolor="#000000", highlightbackground="#000000", borderwidth=0)
+		self.sync_btn.place(y=180, width=100, x=360, height=30)
+		self.sync_btn.bind("<ButtonRelease-1>", lambda event: self.sync_click())
+		self.sync_btn.bind("<Enter>", lambda event: self.sync_btn.config(highlightthickness=4) if not self.started else None)
+		self.sync_btn.bind("<Leave>", lambda event: self.sync_btn.config(highlightthickness=2) if not self.started else None)
+
+		self.syncing_lbl = tk.Label(self.root, text="", font=("Segoe UI", 9, "italic", "bold"),
+		                            anchor=tk.CENTER, justify=tk.CENTER,
+		                            foreground="#000000", activeforeground="#000000", background="#ffffff",
+		                            activebackground="#ffffff", highlightthickness=0, borderwidth=0)
+		self.syncing_lbl.place(height=30, width=100, x=200, y=180)
+
+		self.root.mainloop()
+
+		self.quit()
+
+	def browse_click(self, entry):
+		if not self.started:
+			initial_dir = os.path.dirname(entry.get())
+			if not os.path.isdir(initial_dir):
+				initial_dir = os.path.dirname(sys.executable)
+			if not os.path.isdir(initial_dir):
+				initial_dir = os.path.join(os.path.expanduser('~'), 'Desktop')
+
+			folder = filedialog.askdirectory(parent=self.root, initialdir=initial_dir)
+
+			if folder != "":
+				entry.delete(0, tk.END)
+				entry.insert(0, folder)
+				entry.xview_moveto(1)
+
+	def sync_click(self):
+		if not self.started:
+			source_dir = self.source_txt.get()
+			target_dir = self.target_txt.get()
+
+			not_existing = False
+			if not os.path.exists(source_dir):
+				not_existing = True
+				self.source_txt.delete(0, tk.END)
+			if not os.path.exists(target_dir):
+				not_existing = True
+				self.target_txt.delete(0, tk.END)
+			if not_existing:
+				messagebox.showerror(title="Error!", message="Invalid folder selected!", parent=self.root)
+				return
+
+			if not messagebox.askyesno(title="Mirror",
+			                           message="Running this program will mirror source folder to destination folder. "
+			                                   "Any other files in the destination folder will be deleted.\n"
+			                                   "Are you sure you want to proceed?"):
+				return
+
+			self.started = True
+			self.syncing_lbl.config(text="Mirroring...")
+			self.browse1_btn.config(cursor="arrow", background="grey80",
+			                        activebackground="grey80", highlightthickness=2)
+			self.browse2_btn.config(cursor="arrow", background="grey80",
+			                        activebackground="grey80", highlightthickness=2)
+			self.source_txt.config(state="disabled")
+			self.target_txt.config(state="disabled")
+			self.sync_btn.config(cursor="arrow", background="grey80", activebackground="grey80", highlightthickness=2)
+
+			self.sync_thread = Thread(target=self.syncing, args=(source_dir, target_dir), daemon=True)
+			self.sync_thread.start()
+
+	def syncing(self, source_dir, target_dir):
+		equal_dirs = False
+
+		for _ in range(3):
+			sync(source_dir, target_dir, 'sync', logger=Logger(name="capture-stdout"),
+			     purge=True, force=True, create=True)
+			if are_dir_trees_equal(source_dir, target_dir):
+				equal_dirs = True
+				break
+
+		self.started = False
+		self.syncing_lbl.config(text="")
+		self.browse1_btn.config(cursor="hand2", background="grey95", activebackground="grey95", highlightthickness=2)
+		self.browse2_btn.config(cursor="hand2", background="grey95", activebackground="grey95", highlightthickness=2)
+		self.source_txt.config(state="normal")
+		self.target_txt.config(state="normal")
+		self.sync_btn.config(cursor="hand2", background="grey95", activebackground="grey95", highlightthickness=2)
+
+		if equal_dirs:
+			messagebox.showinfo("Completed!", "Folder was mirrored successfully!", parent=self.root)
 		else:
-			loop_info = False
+			messagebox.showerror("Failed!", "Mirroring failed!", parent=self.root)
 
-	started = False
-	syncing_lbl.config(text="")
-	browse1_btn.config(background="grey95", activebackground="grey95", highlightthickness=2)
-	browse2_btn.config(background="grey95", activebackground="grey95", highlightthickness=2)
-	source_txt.config(state="normal")
-	target_txt.config(state="normal")
-	sync_btn.config(background="grey95", activebackground="grey95", highlightthickness=2)
+	def quit(self):
+		try:
+			self.root.destroy()
+		except _tkinter.TclError:
+			pass
 
-	if loop_info:
-		messagebox.showinfo("Completed!", "Folder was mirrored successfully!", parent=root)
-	else:
-		messagebox.showerror("Failed!", "Mirroring failed!", parent=root)
-
-def sync_click(event):
-	global sync_process, started
-	if not started:
-		sync_process = Thread(target=syncing)
-		sync_process.start()
-
-def exit_click(event=None):
-	global sync_process
-	try:
-		root.destroy()
-	except _tkinter.TclError:
-		pass
-	psutil.Process(os.getpid()).kill()
-
-def browse_click(event, ent):
-	folder = filedialog.askdirectory(parent=root, initialdir=os.getcwd())
-	if folder != "":
-		ent.delete(0, END)
-		ent.insert(0, folder)
-		ent.xview_moveto(1)
-
-def change_thickness(event, widget, typ, t_l, t_h, started):
-	if not started:
-		if typ:
-			widget.config(highlightthickness=t_l)
-		else:
-			widget.config(highlightthickness=t_h)
-
-def change_exit_thickness(event, widget, typ, t_l, t_h):
-	if typ:
-		widget.config(highlightthickness=t_l)
-	else:
-		widget.config(highlightthickness=t_h)
-
-def resource_path(relative_path=""):
-	""" Get absolute path to resource, works for dev and for PyInstaller """
-	try:
-		# PyInstaller creates a temp folder and stores path in _MEIPASS
-		base_path = sys._MEIPASS
-	except Exception:
-		base_path = os.path.abspath(".")
-	return os.path.join(base_path, relative_path)
 
 def main():
-	global started
-	global sync_process
-	global root
-	global browse1_btn, browse2_btn
-	global source_txt, target_txt
-	global syncing_lbl
-	global sync_btn
-
-	started = False
-	sync_process = None
-
-	root = Tk()
-	root.title("DirSync")
-	root.geometry(f"500x230+{root.winfo_screenwidth() // 2 - 250}+{root.winfo_screenheight() // 2 - 115}")
-	root.resizable(False, False)
-	root.iconbitmap(resource_path("data/dir-icon.ico"))
-	root.config(background="#ffffff")
-
-	title_lbl = Label(root, text="DirSync", font=("Times New Roman Bold", 35), anchor=CENTER, justify=CENTER, foreground="#000000", activeforeground="#000000", background="#ffffff", activebackground="#ffffff", highlightthickness=0, borderwidth=0)
-	title_lbl.place(width=500, height=70, x=0, y=0)
-
-	description_lbl = Label(root, text="Mirror source folder to destination folder", font=("Segoe UI", 9, "italic", "bold"), anchor=CENTER, justify=CENTER, foreground="#000000", activeforeground="#000000", background="#ffffff", activebackground="#ffffff", highlightthickness=0, borderwidth=0)
-	description_lbl.place(height=20, width=500, x=0, y=80)
-
-	source_lbl = Label(root, text="Source:", font=("Segoe UI", 10, "bold"), anchor=CENTER, justify=CENTER, foreground="#000000", activeforeground="#000000", background="#ffffff", activebackground="#ffffff", highlightthickness=0, borderwidth=0)
-	source_lbl.place(width=90, height=25, x=0, y=105)
-
-	destination_lbl = Label(root, text="Destination:", font=("Segoe UI", 10, "bold"), anchor=CENTER, justify=CENTER, foreground="#000000", activeforeground="#000000", background="#ffffff", activebackground="#ffffff", highlightthickness=0, borderwidth=0)
-	destination_lbl.place(width=90, height=25, x=0, y=135)
-
-	source_txt = Entry(root, font=("Segoe UI", 9), justify=LEFT, highlightthickness=1, highlightcolor="grey50", highlightbackground="grey50", disabledbackground="grey80", foreground="#000000", background="grey95", borderwidth=0)
-	source_txt.place(width=320, height=25, x=90, y=105)
-
-	target_txt = Entry(root, font=("Segoe UI", 9), justify=LEFT, highlightthickness=1, highlightcolor="grey50", highlightbackground="grey50", disabledbackground="grey80", foreground="#000000", background="grey95", borderwidth=0)
-	target_txt.place(width=320, height=25, x=90, y=135)
-
-	browse1_btn = Label(root, text="Browse", font=("Segoe UI", 9), anchor=CENTER, justify=CENTER, background="grey95", activeforeground="grey95", highlightthickness=2, highlightcolor="#000000", highlightbackground="#000000", borderwidth=0)
-	browse1_btn.place(y=105, width=80, x=415, height=25)
-	browse1_btn.bind("<ButtonRelease-1>", lambda event: browse_click(event, source_txt))
-	browse1_btn.bind("<Enter>", lambda event: change_thickness(event, browse1_btn, False, 2, 4, started))
-	browse1_btn.bind("<Leave>", lambda event: change_thickness(event, browse1_btn, True, 2, 4, started))
-
-	browse2_btn = Label(root, text="Browse", font=("Segoe UI", 9), anchor=CENTER, justify=CENTER, background="grey95", activeforeground="grey95", highlightthickness=2, highlightcolor="#000000", highlightbackground="#000000", borderwidth=0)
-	browse2_btn.place(y=135, width=80, x=415, height=25)
-	browse2_btn.bind("<ButtonRelease-1>", lambda event: browse_click(event, target_txt))
-	browse2_btn.bind("<Enter>", lambda event: change_thickness(event, browse2_btn, False, 2, 4, started))
-	browse2_btn.bind("<Leave>", lambda event: change_thickness(event, browse2_btn, True, 2, 4, started))
-
-	exit_btn = Label(root, text="Quit", font=("Segoe UI", 10, "bold"), anchor=CENTER, justify=CENTER, background="grey95", activeforeground="grey95", highlightthickness=2, highlightcolor="red", highlightbackground="red", borderwidth=0)
-	exit_btn.place(y=180, width=100, x=40, height=30)
-	exit_btn.bind("<ButtonRelease-1>", exit_click)
-	exit_btn.bind("<Enter>", lambda event: change_exit_thickness(event, exit_btn, False, 2, 4))
-	exit_btn.bind("<Leave>", lambda event: change_exit_thickness(event, exit_btn, True, 2, 4))
-
-	sync_btn = Label(root, text="Mirror", font=("Segoe UI", 10, "bold"), anchor=CENTER, justify=CENTER, background="grey95", activeforeground="grey95", highlightthickness=2, highlightcolor="#000000", highlightbackground="#000000", borderwidth=0)
-	sync_btn.place(y=180, width=100, x=360, height=30)
-	sync_btn.bind("<ButtonRelease-1>", sync_click)
-	sync_btn.bind("<Enter>", lambda event: change_thickness(event, sync_btn, False, 2, 4, started))
-	sync_btn.bind("<Leave>", lambda event: change_thickness(event, sync_btn, True, 2, 4, started))
-
-	syncing_lbl = Label(root, text="", font=("Segoe UI", 9, "italic", "bold"), anchor=CENTER, justify=CENTER, foreground="#000000", activeforeground="#000000", background="#ffffff", activebackground="#ffffff", highlightthickness=0, borderwidth=0)
-	syncing_lbl.place(height=30, width=100, x=200, y=180)
-
-	root.mainloop()
-
-	exit_click()
+	App()
 
 
 if __name__ == '__main__':
